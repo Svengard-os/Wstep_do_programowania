a16b72a39684c88235035e10ebfedafbf9c3bfa2
Svengard-os <jangowin99@gmail.com>
2024-10-19 08:25:52 +0200

    zad1

---
+++ lab1.py
@@ -0,0 +1,29 @@
+#Zad. 1:
+
+#1.1. 1 + 2 <class 'int'> Dodawanie
+a = 1 + 2
+print (type(a))
+#1.2. 1 + 4.5 <class 'float'> Dodawanie
+a = 1 + 4.5
+print (type (a))
+#1.3. 3 / 2 <class 'float'> Dzielenie zmiennoprzecinkowe
+a = 3/2
+print(type(a))
+#1.4. 4 / 2 <class 'float'> Dzielenie zmiennoprzecinkowe
+a = 4 / 2
+print (type(a))
+#1.5. 3 // 2 <class 'int'> Dzielenie całkowite
+a = 3 // 2
+print(type(a))
+#1.6. -3 // 2 <class 'int'> Dzielenie całkowite
+a = -3 // 2
+print (type(a))
+#1.7. 11 % 2 <class 'int'> Dzielenie modulo (reszta z dzielenia)
+a = 11 % 2
+print (type(a))
+#1.8. 2 ** 10 <class 'int'>  Potęgowanie
+a = 2** 10
+print(type(a))
+#1.9. 8 ** (1/3) <class 'float'> Potęgowanie
+a = 8 ** (1/3)
+print (type(a))
